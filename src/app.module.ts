7bebb0a1c9845066163cbc1bd1f3406f117de250
Rohhab <s.pooria.hosseini@gmail.com>
2025-07-21 21:34:11 +0330

    Add bank-accounts module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TransactionsModule } from './transactions/transactions.module';
+import { BankAccountsModule } from './bank-accounts/bank-accounts.module';
 
 @Module({
-  imports: [UsersModule, TransactionsModule],
+  imports: [UsersModule, TransactionsModule, BankAccountsModule],
   controllers: [AppController],
   providers: [AppService],
 })
