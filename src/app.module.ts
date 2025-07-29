a95b0199ae7ed4b245dea2c4e9f532ca415774dd
Rohhab <s.pooria.hosseini@gmail.com>
2025-07-29 21:19:28 +0330

    Add Passport and TypeORM modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,25 @@ import { UsersModule } from './users/users.module';
 import { TransactionsModule } from './transactions/transactions.module';
 import { BankAccountsModule } from './bank-accounts/bank-accounts.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/entities/user.entity';
 
 @Module({
-  imports: [UsersModule, TransactionsModule, BankAccountsModule, AuthModule],
+  imports: [
+    UsersModule,
+    TransactionsModule,
+    BankAccountsModule,
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 8080,
+      username: 'Pouri',
+      password: 'A@123456',
+      database: 'finance-log',
+      entities: [User],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
