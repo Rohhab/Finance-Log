4cca44d60963abd5cfc15d228f8332aea136e8e8
Rohhab <s.pooria.hosseini@gmail.com>
2025-10-17 19:01:31 +0330

    Add mock CRUD methods to bank accounts controller

--- src/bank-accounts/bank-accounts.controller.ts
+++ src/bank-accounts/bank-accounts.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 
 @Controller('bank-accounts')
-export class BankAccountsController {}
+export class BankAccountsController {
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  getAllBankAccounts() {
+    return 'Hello Bank Account!';
+  }
+
+  @Get(':id')
+  @HttpCode(HttpStatus.OK)
+  getOneBankAccount(@Param('id') id: string) {
+    return Number(id);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  createBankAccount() {}
+
+  @Put()
+  @HttpCode(HttpStatus.OK)
+  updateBankAccount() {}
+
+  @Delete()
+  deleteBankAccount() {}
+}
