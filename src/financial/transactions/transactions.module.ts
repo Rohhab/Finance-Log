297ccd37435be567e3cedfba7c3c80cd4cc683a0
Rohhab <s.pooria.hosseini@gmail.com>
2025-10-24 01:20:59 +0330

    Add Transaction Controller and Service

--- src/financial/transactions/transactions.module.ts
+++ src/financial/transactions/transactions.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Transaction } from './entities/transaction.entity';
 import { Category } from './entities/category.entity';
+import { TransactionService } from './transactions.service';
+import { TransactionsController } from './transactions.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Transaction, Category])],
+  providers: [TransactionService],
+  controllers: [TransactionsController],
 })
 export class TransactionsModule {}
